d13b1ec8abd18622e79a1ba400a6c98d327b27e2
dev-hajariwala <devhajariwala1102@gmail.com>
2024-07-14 11:55:23 +0530

    this is mt last commit

---
+++ dh.py
@@ -0,0 +1,6 @@
+a=int(input("Enter Your age"))
+print("Your Age is ",a)
+if(a>18):
+    print("You Can vote now")
+else:
+    print("You Can not vote now")
---
+++ elif.py
@@ -0,0 +1,7 @@
+num=int(input("Enter Your Number\n"))
+if(num<0):
+    print("Your Number is Negetive")
+elif(num==0):
+    print("Your Number is Zero")
+else:
+    print("Your Number is Positive")
