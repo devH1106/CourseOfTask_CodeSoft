b2947982da12003bfbf7bf7978ea8a23dcb9890a
devH1106 <devhajariwala2605@gmail.com>
2024-07-22 22:16:04 +0530

    Delete sd.py

--- sd.py
+++
@@ -1,8 +0,0 @@
-
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
-
-print("The first number is ",num1,"+","Second number is" , num2, "=", (num1+ num2))
-print("The first number is ",num1, "-","Second number is" , num2, "=",( num1- num2))
-print("The first number is ",num1, "*","Second number is" , num2, "=",(num1* num2))
-print("The first number is ",num1, "/","Second number is" , num2, "=",( num1/ num2))
